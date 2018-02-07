8c10cf3d4b9d2e553300a42579e5b970ac86b4df
Daniel <danielbrogers594@gmail.com>
2018-02-07 18:27:47 -0500

    first commit

---
+++ Class2Prep1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class2Prep1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>        
+            {
+                2,
+                4,
+                6,
+                8,
+                10,
+                12,
+                14,
+                16,
+                18,
+                20
+            };
+
+            int[] intArray = intList.ToArray();
+
+
+            int sum = 0;
+            Array.ForEach(intArray, delegate (int i) { sum += i; });
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+
+        }
+       
+
+    }
+}
